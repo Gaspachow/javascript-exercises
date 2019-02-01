8c983d1b04389a39ab7b1c229b397d5076c85187
Gas <gaspardsmets@gmail.com>
2019-02-01 20:48:44 +0100

    Solved removeFromArray.js exercice

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,toDelete1, toDelete2, toDelete3, toDelete4) {
 
+    for (i=0;i<array.length;i++) {
+        if (array[i] === toDelete1 || array[i] === toDelete2 || array[i] === toDelete3 || array[i] === toDelete4) {
+            array.splice(i,1);
+            i--;
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
