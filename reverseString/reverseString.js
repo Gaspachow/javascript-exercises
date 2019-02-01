df58214584b225d56095c5279aac650c5f51bd5d
Gas <gaspardsmets@gmail.com>
2019-02-01 20:16:16 +0100

    Solved reverseString exercice

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(s) {
+let string = s.split('');
+let answer = '';
+
+for (i=s.length-1;i>=0;i--) {
+    answer +=string[i]; 
+}
+
+return answer;
 
 }
 
