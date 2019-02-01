e7d3aa83e8864178277b2e3159d1edad2c43d851
Gas <gaspardsmets@gmail.com>
2019-02-01 19:52:41 +0100

    Solved repeatString exercice

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text,number) {
+    let answer='';
+    if (number < 0) {
+        return "ERROR";
+    } else {
+        for (i=1;i<=number;i++) {
+            answer += text;
+        }
+        return answer;
+    }
 }
 
 module.exports = repeatString
