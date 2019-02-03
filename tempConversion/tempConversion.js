a36f97390e0c4f520ca9ca3186d06d08b40cc498
Gas <gaspardsmets@gmail.com>
2019-02-03 16:09:47 +0100

    Solved tempConversion Exercice

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let c = ((far-32)*5/9)
+  return Math.round(c*10)/10;
 }
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let f = ((cel*9/5)+32)
+  return Math.round(f*10)/10;
 }
 
 module.exports = {
