c2e83aeca31f8d56fad834d57c4e3195cd172919
Gas <gaspardsmets@gmail.com>
2019-02-03 15:34:55 +0100

    Solved sumAll exercice

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
-const removeFromArray = function(array,toDelete1, toDelete2, toDelete3, toDelete4) {
+const removeFromArray = function(array, toDelete1, 
+                                        toDelete2, 
+                                        toDelete3, 
+                                        toDelete4) {
 
     for (i=0;i<array.length;i++) {
-        if (array[i] === toDelete1 || array[i] === toDelete2 || array[i] === toDelete3 || array[i] === toDelete4) {
+        if (array[i] === toDelete1 ||
+             array[i] === toDelete2 ||
+              array[i] === toDelete3 ||
+               array[i] === toDelete4) {
             array.splice(i,1);
             i--;
         }
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let answer=0;
+    let start, finish;
+    if (num1 < 0 || num2 < 0 || !(typeof num1 === 'number') || !(typeof num2 === 'number')) {
+        answer='ERROR'
+    } else {
+        if (num1 < num2) {
+            start=num1;
+            finish=num2;
+        } else {
+            start=num2;
+            finish=num1;
+        }
 
+        for (i=start; i <= finish; i++) {
+            answer += i;
+        }
+    }
+    return answer;
 }
 
 module.exports = sumAll
